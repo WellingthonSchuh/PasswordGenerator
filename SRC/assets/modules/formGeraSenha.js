6246f0a4b2532b084be68bb6f5562073e2f5599f
WellingthonSchuh <140724607+WellingthonSchuh@users.noreply.github.com>
2025-11-17 15:07:56 -0300

    add files

---
+++ SRC/assets/modules/formGeraSenha.js
@@ -0,0 +1,28 @@
+import geraSenha from "./geradores";
+
+const senhaGerada = document.querySelector('.resultado');
+const qtdCaracteres = document.querySelector('.qtd-caracteres');
+const chkMaiusculas = document.querySelector('.chk-maiusculas');
+const chkMinusculas = document.querySelector('.chk-minusculas');
+const chkNumeros = document.querySelector('.chk-numeros');
+const chkSimbolos = document.querySelector('.chk-simbolos');
+const gerar = document.querySelector('.gerar-senha');
+
+
+
+export default () => {
+    gerar.addEventListener('click', () => {
+        senhaGerada.innerHTML = gera();
+    })
+};
+
+function gera() {
+    const senha = geraSenha(
+        qtdCaracteres.value, 
+        chkMaiusculas.checked,
+        chkMinusculas.checked,
+        chkNumeros.checked,
+        chkSimbolos.checked
+    );
+    return senha || 'Nada selecionado';
+}
